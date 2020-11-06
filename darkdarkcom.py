a8e999d261fcd67f87040bc237100c82d5641420
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2020-11-06 10:13:35 -0500

    main

---
+++ darkdarkcom.py
@@ -0,0 +1,22 @@
+print("----------")
+print("Welcome to Doc Darks!")
+x = input("")
+print("1: Write 2: View 3: Leave")
+biu = input("@User: ")
+if biu == "1":
+  print("Enter things in to the input below! Like PRIVATE data!")
+  data = input("@User: ")
+  f = open("dark.docs.data.txt", "a")
+  f.write(data)
+  f.close()
+  import darkdarkcom
+if biu == "2":
+	f = open("demofile2.txt", "r")
+	print(f.read())
+	input("")
+	import darkdarkcom
+if biu == "3":
+	import WEBDIFINE
+
+
+  
